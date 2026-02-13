8ebe844a5d5ca1b7406280ffde02273308eaf476
AngusF22 <3074253+AngusF22@users.noreply.github.com>
2026-02-12 17:33:51 -0700

    Create VolumeDown1.vb

---
+++ VolumeDown1.vb
@@ -0,0 +1,5 @@
+dim xml = API.XML()
+dim x as new system.xml.xmldocument
+x.loadxml(xml)
+dim DynamicInput1 as String = (x.SelectSingleNode("/vmix/dynamic/input1").InnerText)
+API.Function("SetVolume", Input:=DynamicInput1, Value:="-=1")
